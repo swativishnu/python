643ff20ef0581d37ef65b16692265cec49a90dab
swativishnu <swativishnu@gmail.com>
2022-08-04 15:13:26 +0530

    for demo

---
+++ for.py
@@ -0,0 +1,13 @@
+# numbers= [1,2,3,4,5]
+# for item in numbers:
+#     print(item)
+numbers=range(0,5)
+for number in  numbers:
+    print(number)
+numbers=range(5,10,2)
+for number in  numbers:
+    print(number)
+
+numbers=range(5)
+for number in  numbers:
+    print(number)
