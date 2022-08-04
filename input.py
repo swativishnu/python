ce05a96e458d3c571082f948d20878c6b496e56a
swativishnu <swativishnu@gmail.com>
2022-08-04 12:26:06 +0530

    input and type conversion

---
+++ input.py
@@ -0,0 +1,12 @@
+name = input("Enter your name")
+print("Hello  " + name)
+
+Birth_year = input("Enter your birth year")
+age = 2022 -int(Birth_year)
+print(age)
+
+first = input("First: ")
+second = input("Second: ")
+sum = float(first) + float(second)
+print("Sum: " + str(sum))
+
