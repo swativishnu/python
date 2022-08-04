e1918c2e0913bbdab65a720a1452639885dea1e8
swativishnu <swativishnu@gmail.com>
2022-08-04 15:01:40 +0530

    While and list demo

---
+++ list.py
@@ -0,0 +1,16 @@
+names = [ 'John','Bob','Mosh','Sam', 'Marry']
+print(names)
+print(names[-2])
+names[0]= "Jon"
+print(names[0:3])
+numbers= [1,2,3,4,5]
+numbers.insert(0,0)
+print(numbers)
+numbers.remove(3)
+print(numbers)
+print( 1 in numbers)
+
+#numbers.clear()
+print(numbers)
+print(10 in numbers)
+print(len(numbers))
