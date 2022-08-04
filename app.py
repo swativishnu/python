b3d5bc753825502947eb4d5addb753270f3bf212
swativishnu <swativishnu@gmail.com>
2022-08-04 11:57:24 +0530

    patient file added

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 age =20
+price =19.5
+first_name= "Mosh"
+is_online = False
 print("Hello world")
 print(age)
---
+++ patient.py
@@ -0,0 +1,4 @@
+Patient_name = "John Smit"
+age =20
+is_New = True
+print(Patient_name,age,is_New)
